85ff04c0a5646cd6816f6640c20da091ee3f491f
jisoooo17 <jisoooo17@gmail.com>
2024-09-08 13:55:12 +0900

    리스트 삭제 및 수정 기능 작업중

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,19 +1,21 @@
 import { Button, Input } from 'antd';
 import React from 'react';
 
-interface TodoItem {
+interface TodoItemProps {
+  id: number;
   text: string;
   completed: boolean;
+  onClkDel(id: number): void;
 }
 
-const TodoItem = ({text, completed}: TodoItem) => { //  props를 타입에 맞게 지정
+const TodoItem = ({id, text, completed, onClkDel}: TodoItemProps) => { //  props를 타입에 맞게 지정
   return (
     <li className="list">
       {completed ? <Button type="primary">완료됨</Button> : <Button type="primary">완료하기</Button>}
       <p className="txt">{text}</p>
       <div className="btn-w">
         <Button htmlType="button" className="btn-adit">수정</Button>
-        <Button htmlType="button" className="btn-del">삭제</Button>
+        <Button htmlType="button" className="btn-del" onClick={()=> onClkDel(id)}>삭제</Button>
       </div>
     </li>
   );
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -23,20 +23,43 @@ const TodoList = () => {
     }
   ]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+   // 입력 필드에 텍스트가 변경될 때 호출되는 함수
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputTxt(e.target.value);
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void=>{
+  // 리스트 추가할 때 호출되는 함수
+  const inputSubmit = (e: React.FormEvent<HTMLFormElement>): void=>{
     e.preventDefault();
+
+    // 새로운 할 일 항목 생성
     const newTodo: Todo = {
-      id: Date.now(),
-      text: inputTxt,
+      id: Date.now(), // 현재 시간을 기반으로 고유 ID 생성
+      text: inputTxt, // 입력된 텍스트를 사용
       completed: false,
     }
 
     setList([...list, newTodo]);
     setInputTxt("");
+  }
+
+
+  // 리스트 삭제
+  const listDelete = (id: number) => {
+    setList(list.filter((item) => item.id !== id));
+  }
+
+  // 리스트 수정
+  const listEdit = (newTodo: Todo): void => { // newTodo는 새롭게 입력한 값
+    const newTodoList = list.map((item)=>{
+
+      if (item.id === newTodo.id){ // id값이 같으면 새롭게 입력한 값으로 return
+        return newTodo;
+      } else { // 그 외에는 기존 값 return
+        return item
+      }
+    });
+    setList(newTodoList);
   }
   
   return (
@@ -45,12 +68,12 @@ const TodoList = () => {
         {
           list.map((item, i) => {
             return(
-              <TodoItem key={item.id} text={item.text} completed={item.completed}/>
+              <TodoItem key={item.id} id={item.id} text={item.text} completed={item.completed} onClkDel={listDelete}/>
             )
           })
         }
       </ul>
-      <CreateTodo onChange={handleInputChange} onSubmit={handleSubmit} inputTxt={inputTxt}/>
+      <CreateTodo onChange={inputChange} onSubmit={inputSubmit} inputTxt={inputTxt}/>
     </div>
   );
 };
