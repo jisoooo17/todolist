728bceee4f02a60e1dbf22f0491780c76012bdad
jisoooo17 <jisoooo17@gmail.com>
2024-09-08 21:36:51 +0900

    빈 값일 경우 alert 띄우기

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -7,10 +7,20 @@ interface CreateTodoProps {
 }
 
 const CreateTodo = ({onChange, onSubmit, inputTxt}: CreateTodoProps) => {
+  // 인풋이 빈 값이면 alert 띄움
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(inputTxt.trim()===""){
+      alert("할 일을 입력해주세요.")
+    } else {
+      onSubmit(e);
+    }
+  }
 
   return (
     <div className="input-area">
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit}>
         <div className="input-w">
           <Input type="text" value={inputTxt} onChange={onChange}  placeholder="할 일을 입력하세요." />
           <Button htmlType="submit" type="primary" className="btn-register">등록</Button>
